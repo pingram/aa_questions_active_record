4745e5e988b5a9f3575a4354f9a29f89233c431b
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-20 19:58:38 -0700

    finished hard queries, questionlikes left

--- app/models/question_follower.rb
+++ app/models/question_follower.rb
@@ -1,4 +1,13 @@
 class QuestionFollower < ActiveRecord::Base
 	belongs_to :question
 	belongs_to :follower, class_name: "User", foreign_key: :follower_id
+
+	def self.most_followed_questions(n)
+		Question
+			.select("questions.id, COUNT(question_followers.id) AS num_follows")
+			.joins("LEFT OUTER JOIN question_followers ON questions.id = question_followers.question_id")
+			.group("questions.id")
+			.order("COUNT(question_followers.id) DESC")
+			.limit(n)
+	end
 end
