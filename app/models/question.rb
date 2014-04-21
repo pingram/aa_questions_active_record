69433c0a9bf8d62fb8cd55fbdb99def261bfd3c1
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-20 19:46:16 -0700

    medium queries

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
 	belongs_to :author, class_name: "User", foreign_key: :author_id
 	has_many :replies
+	has_many :question_followers
+	has_many :followers, through: :question_followers
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :authored_questions, class_name: "Question", foreign_key: :author_id
 	has_many :authored_replies, class_name: "Reply"
+	has_many :question_follows, class_name: "QuestionFollower", foreign_key: :follower_id
+	has_many :followed_questions, through: :question_follows, source: :question
 
 	def self.find_by_name(fname, lname)
 		self.find_by(fname: fname, lname: lname)
